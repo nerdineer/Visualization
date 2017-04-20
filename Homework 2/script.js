637b5ace2725163c5948932769d6011233b4eea2
Melissa Sheridan <sheridme@oregonstate.edu>
2017-04-20 13:47:04 -0700

    All the files we have so far
    This might get messy but the projects are simple so far

---
+++ Homework 2/script.js
@@ -0,0 +1,17 @@
+var data = [4, 8, 15, 20, 30, 42];
+
+var x = 40
+
+var chart = d3.select(".chart");
+chart.style("width",720);
+chart.style("height",120)
+
+
+var circle = chart.selectAll("circle");
+var circleUpdate = circle.data(data);
+var circleEnter = circleUpdate.enter().append("circle");
+circleEnter.style("fill", "steelblue")
+circleEnter.attr("cx",function(d) { return x+=80; });
+circleEnter.attr("cy",60);
+circleEnter.attr("r",function(d) { return d; });
+circleEnter.text(function(d) { return d; });
