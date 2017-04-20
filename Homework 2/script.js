85e7f46e22faba64fe99f983c035366385585194
Melissa Sheridan <sheridme@oregonstate.edu>
2017-04-20 15:11:56 -0700

    Just some slight changes

--- Homework 2/script.js
+++ Homework 2/script.js
@@ -6,12 +6,14 @@ var chart = d3.select(".chart");
 chart.style("width",720);
 chart.style("height",120)
 
-
 var circle = chart.selectAll("circle");
 var circleUpdate = circle.data(data);
-var circleEnter = circleUpdate.enter().append("circle");
-circleEnter.style("fill", "steelblue")
+var circleEnter = circleUpdate.enter().append('circle');
+circleEnter.style("color", "white");
+circleEnter.style("fill", "steelblue");
 circleEnter.attr("cx",function(d) { return x+=80; });
 circleEnter.attr("cy",60);
 circleEnter.attr("r",function(d) { return d; });
-circleEnter.text(function(d) { return d; });
+
+
+
