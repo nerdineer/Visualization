5a1b4b601872a30a957f32be176d3dbbb7ce4128
EmmittJ <1337ERJ1337@gmail.com>
2017-04-25 15:24:16 -0700

    Added some code to reduce the size of the json (limit to Saturday) Added the raw json file (no longer needs zipped)

--- Data/twitch_db_to_json.py
+++ Data/twitch_db_to_json.py
@@ -18,7 +18,7 @@ for i in cursor:
         user[int(i[0])] = i[1]
 #print user
 
-cursor.execute("SELECT * FROM message")
+cursor.execute("select * from message where timestamp between  1492214400 and 1492300800")
 message = []
 for i in cursor:
     try: 
@@ -30,5 +30,15 @@ for i in cursor:
 
 mariadb_connection.close()
 
-json_dump = [channel, user, message]
+real_users = {}
+real_channels = {}
+for j in message:
+    if j['userid'] in user:
+        real_users[j['userid']] = user[j['userid']]
+    if j['channelid'] in channel:
+        real_channels[j['channelid']] = channel[j['channelid']]
+        
+#print "New vs. Old User count: ", len(real_users), len(user)
+#print "New vs. Old Channel count: ", len(real_channels), len(channel)
+json_dump = [real_channels, real_users, message]
 print json.dumps(json_dump)
